a32f07400c51499d42c32ec63ac9960152821d6b
rawanazazi12 <rawanazazi12@gmail.com>
2021-08-11 15:53:26 +0300

    having an error

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -1,6 +1,6 @@
 const {userModel}=require('../models/user.model');
 
-const getBooks=async=(req,res)=>{
+const getBooks=async(req,res)=>{
     const {email}=req.query;
 
     userModel.findOne({ email: email }, (error, user) => {
@@ -9,10 +9,32 @@ const getBooks=async=(req,res)=>{
           res.send('no data was found');
         } else {
           res.json(user.books);
-          console.log(user);
+          // console.log(user.books);
         }
       });
+};
+
+const createBook=async(req,res)=>{
+    console.log(req.body);
+  const title=req.body.title;
+  const description=req.body.description;
+  const status=req.body.status;
+  const email=req.body.email;
+
+  // const{
+  //   title,description,status,email
+  // }=req.body;
+  const newBookObj=new userModel({
+    title,
+    description,
+    status,
+    email
+  });
+  
+  newBookObj.save();
+  res.json(newBookObj);
 }
 
 
-module.exports={getBooks};
+
+module.exports={getBooks, createBook};
